8a50c9b8831a9fb09de651f54e2fd70e0bf3125b
Riou <dylanriou07500@gmail.com>
2024-12-18 15:31:34 +0100

    m

--- assets/js/arrayveille.js
+++ assets/js/arrayveille.js
@@ -1,11 +1,10 @@
 console.log("début arrey")
 
 const veillearticles = [
-    { titre: "Produit A", prix: 19.99, description: "Un excellent produit pour commencer." },
-    { titre: "Produit B", prix: 29.99, description: "Un produit de qualité pour les amateurs." },
-    { titre: "Produit C", prix: 39.99, description: "Idéal pour les professionnels." },
-    { titre: "Produit D", prix: 49.99, description: "Un produit haut de gamme pour les connaisseurs." },
-    { titre: "Produit E", prix: 59.99, description: "Le top du top pour les experts." }
+    { titre: "connexion 5G", description: "une exellente rapidité" },
+    { titre: "licence", description: "les opérateurs doivent obtenir plusieurs licence" },
+    { titre: "surveillance", description: "Les antennes sont surveillées régulièrement" },
+    { titre: "consommateurs", description: "avantage de la 5G et comment en bénéficier" },
   ];
   // Sélectionne l'élément avec l'ID "listedupanier" pour insérer les produits
 const listedupanier = document.getElementById("listedupanier");
@@ -17,7 +16,6 @@ veillearticles.forEach(produit => {
     <div class="card mb-3" style="width: 18rem;">
       <div class="card-body">
         <h5 class="card-title">${produit.titre}</h5>
-        <h6 class="card-subtitle mb-2 text-muted">Prix: ${produit.prix}€</h6>
         <p class="card-text">${produit.description}</p>
       </div>
     </div>
